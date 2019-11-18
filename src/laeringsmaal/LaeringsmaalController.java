e060c048bf950cb073f6c95b3268dc59d52f5175
sune <sune1309@edu.easj.dk>
2019-11-18 10:33:12 +0100

    hej

--- src/laeringsmaal/LaeringsmaalController.java
+++ src/laeringsmaal/LaeringsmaalController.java
@@ -8,9 +8,12 @@ import model.Sporgsmaal;
 public class LaeringsmaalController {
 
     private int i1 = 0;
+    private int j1 = 0;
     Emner emner = new Emner();
     Sporgsmaal q = new Sporgsmaal();
-    private int j = q.getAlleSporgsmaal().size();
+   private int j2 = q.getAlleSporgsmaal().size();
+    private int i2 = emner.getEmner().length;
+
 
 
 
@@ -22,24 +25,38 @@ public class LaeringsmaalController {
 
     @FXML
     private void indlaes() {
-        if (i1>=0 || i1>=10) {
+        if (i1>=0 || i1<=i2) {
             System.out.println(emner.getEmne(i1));
-            emneTextField.setText(emner.getEmne(i1)1);
-            spoergsmaalTextArea.setText(q.getSporgsmaal(i1) + "\n");
+            emneTextField.setText(emner.getEmne(i1));
+            if(j1>=0 ||j1<j2) {
+            spoergsmaalTextArea.setText(q.getSporgsmaal(i1) + "\n");}
         }
+
     }
 
     @FXML
     private void naeste() {
-        i1++;
-        indlaes();
+        if (j1<j2-1) {
+            i1++;
+            j1++;
+            indlaes();
+        }
     }
 
     @FXML
     private void forrige() {
-        i1--;
-        emneTextField.setText("");
-        indlaes();
+        if (j1<0 && i1<0) {
+            i1--;
+            j1--;
+            emneTextField.setText("");
+            indlaes();
+        }
+    }
+
+    @FXML
+    private void gem() {
+        emner.setEmne(i1, emneTextField.getText());
+        q.setSporgsmaal(j1, spoergsmaalTextArea.getText());
     }
 
 }
